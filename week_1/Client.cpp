5ade57e8124ba61ff35096a80f6b86368d12a36b
carlos chicata <carlos.fernando.chicata@gmail.com>
2021-03-30 15:29:43 -0500

    fix: add code in main

--- week_1/Client.cpp
+++ week_1/Client.cpp
@@ -55,3 +55,56 @@ void procesThread(int ip_conn){
 
 
 int main(){
+    struct sockaddr_in stSockAddr;
+    int Res;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int n;
+
+ 
+    /// is socket created?
+    if (-1 == SocketFD)
+    {
+      perror("cannot create socket");
+      exit(EXIT_FAILURE);
+    }
+ 
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(PORT);
+    Res = inet_pton(AF_INET, IP_CLIENT.c_str(), &stSockAddr.sin_addr);
+ 
+    /// is params ok?
+    if (0 > Res)
+    {
+      perror("error: first parameter is not a valid address family");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    else if (0 == Res)
+    {
+      perror("char string (second parameter does not contain valid ipaddress");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+
+    /// is connection ok?
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+      perror("connect failed");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+
+    /* perform read write operations ... */
+    thread ClientSender(procesThread, SocketFD);
+    ClientSender.join();
+
+    for(;;){
+    }
+
+    shutdown(SocketFD, SHUT_RDWR);
+ 
+    close(SocketFD);
+    return 0;
+}
